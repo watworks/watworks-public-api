84cf10ed9fdb854a299ce0cd083581a4c81e3899
Evan Villemez <evillemez@gmail.com>
2017-06-30 11:26:13 -0400

    updated main

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/watworks/watworks-public-api/api"
 	"github.com/watworks/watworks-public-api/services"
 )
 
 func main() {
-
-	// TODO: load a config file!  This will be fun.  If it doesn't load, must exit
-	if false {
-		log.Fatal("no config file")
-	}
-
+	// get config and create the server
 	c := services.Config()
-	http.ListenAndServe(c.ServerUrl, api.NewRouter())
+	s := &http.Server{
+		Addr:    c.ServerUrl,
+		Handler: api.NewRouter(),
+	}
+	done := make(chan os.Signal, 2)
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
 
-	log.Println("Running on :80...")
+	// listen for interrupt to shutdown the server
+	go func() {
+		<-done
+		fmt.Print("Shutting down...")
+		if err := s.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// run server... will be closed from goroutine on interrupt
+	fmt.Println("Started!")
+	s.ListenAndServe()
+	fmt.Print(" done!\n")
+	os.Exit(0)
 }
